7fdcee1e4dbd84f19291d97bf81421b280a71dba
naokikoseki <naoki.koseki0220@gmail.com>
2020-09-12 14:23:27 +0900

    ActiveHashの導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :users
-  resources :items
-  get 'new', to: 'items#new'
+  resources :items  do
     resources :comments, only: :create
+  end
 end
