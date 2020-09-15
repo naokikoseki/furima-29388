3e8c034945b7f1831510e1c11be5ffe19eacf1a9
naokikoseki <naoki.koseki0220@gmail.com>
2020-09-15 18:49:12 +0900

    エラーが起きた際の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,8 +25,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    redirect_to root_path
+    if @item.update(item_params)
+      return redirect_to root_path
+    else
+      render 'items/edit'
+    end
   end
 
   private
