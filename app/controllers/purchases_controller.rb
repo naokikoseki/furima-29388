82f7dfbc7a5428fedb378639e1d1fab88fdced68
naokikoseki <naoki.koseki0220@gmail.com>
2020-09-17 15:47:27 +0900

    payjpの導入

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,7 @@ class PurchasesController < ApplicationController
   def index
     if user_signed_in?
     @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
     else
       redirect_to 'devise/sessions/new'
     end
@@ -12,11 +13,27 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    @purchase = Purchase.new
-    if @purchase.save
-      return render 'root_path'
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      return redirect_to root_path
     else
       redirect_to action: :index
     end
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: purchase_params[:price],  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,7 @@ class Purchase < ApplicationRecord
   belongs_to :item
   belongs_to :user
   has_one :address
+
+  attr_accessor :token
+  validates :token, presence: true
 end
